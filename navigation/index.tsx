2d67acf579feeac04fc7cac32dc2a98f530f0d9c
Igor Shadurin <igor.shadurin@gmail.com>
2022-01-23 14:08:46 +0300

    main page template

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -18,15 +18,16 @@ import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
-  return (
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <RootNavigator />
-    </NavigationContainer>
-  );
+export default function Navigation({colorScheme}: { colorScheme: ColorSchemeName }) {
+    return (
+        <NavigationContainer
+            linking={LinkingConfiguration}
+            theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <RootNavigator/>
+        </NavigationContainer>
+    );
 }
 
 /**
@@ -36,15 +37,15 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-      <Stack.Group screenOptions={{ presentation: 'modal' }}>
-        <Stack.Screen name="Modal" component={ModalScreen} />
-      </Stack.Group>
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Root" component={BottomTabNavigator} options={{headerShown: false}}/>
+            <Stack.Screen name="NotFound" component={NotFoundScreen} options={{title: 'Oops!'}}/>
+            <Stack.Group screenOptions={{presentation: 'modal'}}>
+                <Stack.Screen name="Modal" component={ModalScreen}/>
+            </Stack.Group>
+        </Stack.Navigator>
+    );
 }
 
 /**
@@ -54,54 +55,56 @@ function RootNavigator() {
 const BottomTab = createBottomTabNavigator<RootTabParamList>();
 
 function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme();
 
-  return (
-    <BottomTab.Navigator
-      initialRouteName="TabOne"
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme].tint,
-      }}>
-      <BottomTab.Screen
-        name="TabOne"
-        component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate('Modal')}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
-          ),
-        })}
-      />
-      <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoScreen}
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
+    return (
+        <BottomTab.Navigator
+            initialRouteName="TabOne"
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme].tint,
+            }}>
+            <BottomTab.Screen
+                name="TabOne"
+                component={TabOneScreen}
+                options={({navigation}: RootTabScreenProps<'TabOne'>) => ({
+                    title: 'Wallet',
+                    tabBarIcon: ({color}) => <TabBarIcon name="wallet-outline" color={color}/>,
+                    headerRight: () => (
+                        <Pressable
+                            onPress={() => navigation.navigate('Modal')}
+                            style={({pressed}) => ({
+                                opacity: pressed ? 0.5 : 1,
+                            })}>
+                            {/*<FontAwesome*/}
+                            {/*  name="info-circle"*/}
+                            {/*  size={25}*/}
+                            {/*  color={Colors[colorScheme].text}*/}
+                            {/*  style={{ marginRight: 15 }}*/}
+                            {/*/>*/}
+                            <Ionicons name="qr-code-outline" size={30} color="black" style={{marginRight: 15}}/>
+                        </Pressable>
+                    ),
+                })}
+            />
+            <BottomTab.Screen
+                name="TabTwo"
+                component={TabTwoScreen}
+                options={{
+                    title: 'DApps',
+                    tabBarIcon: ({color}) => <TabBarIcon name="code-outline" color={color}/>,
+                }}
+            />
+        </BottomTab.Navigator>
+    );
 }
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
+    name: React.ComponentProps<typeof FontAwesome>['name'];
+    color: string;
 }) {
-  return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
+    // return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
+    return <Ionicons size={30} style={{marginBottom: -3}} {...props} />;
 }
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,127 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import { Dimensions, ImageBackground, StyleSheet } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { Ionicons } from '@expo/vector-icons';
+import balanceBack from '../assets/images/wallet-back.png';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+export default function TabOneScreen({}: RootTabScreenProps<'TabOne'>) {
+    const balances = {
+        xdai: '9999.999999999999999999',
+        xbzz: '9999.9999999999999999'
+    };
+
+    const prepareBalance = (balance: string) => {
+        const split = balance.split('.')
+        if (split.length === 2 && split[1].length > 2) {
+            return `${split[0]}.${split[1].substr(0, 2)}`;
+        } else {
+            return balance;
+        }
+    }
+    return (
+        <View style={styles.container}>
+            {/*<Text style={styles.title}>Tab One</Text>*/}
+            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
+
+            <View style={styles.balanceContainer}>
+                <ImageBackground source={balanceBack}
+                                 imageStyle={{borderRadius: 16}}
+                                 resizeMode='cover'
+                                 style={styles.balanceBack}>
+                    <Text style={styles.balanceText}>{prepareBalance(balances.xdai)} xDai</Text>
+                    <Text style={styles.balanceTextSmall}>{prepareBalance(balances.xbzz)} xBzz</Text>
+                </ImageBackground>
+            </View>
+
+            <View style={styles.walletButtonsHolder}>
+                <View style={styles.walletButton}>
+                    <Ionicons style={styles.walletIcon} name="arrow-down-circle-outline" size={60} color="black"/>
+                    <Text style={styles.walletButtonTitle}>Receive</Text>
+                </View>
+                <View style={styles.walletButton}>
+                    <Ionicons style={styles.walletIcon} name="arrow-up-circle-outline" size={60} color="black"/>
+                    <Text style={styles.walletButtonTitle}>Send</Text>
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    separator: {
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+    },
+    balanceContainer: {
+        // width:'100%',
+        flex: 1,
+        flexDirection: 'column'
+    },
+    balanceText: {
+        marginLeft: 10,
+        color: 'white',
+        fontSize: 40
+    },
+    balanceTextSmall: {
+        marginLeft: 10,
+        color: 'white',
+        fontSize: 30
+    },
+    balanceBack: {
+        // flex: 1,
+        justifyContent: 'center',
+        // textAlignVertical:'center',
+        // alignItems:'center',
+        width: Dimensions.get('window').width * 0.9,
+        height: 180,
+        padding: 10
+    },
+    walletButtonsHolder: {
+        marginTop: 20,
+        justifyContent: 'space-between',
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    walletButtonTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    walletIcon: {
+        textAlign: 'center'
+    },
+    walletButton: {
+        padding: 8,
+        marginLeft: 5,
+        marginRight: 5,
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderColor: '#d7d7d7',
+        borderRadius: 16,
+        width: 150,
+        height: 120,
+    },
+    // balanceCard:{
+    //     // backgroundColor:'yellow',
+    //     padding: 8,
+    //     marginLeft: 5,
+    //     marginRight: 5,
+    //     borderStyle: 'solid',
+    //     borderWidth: 1,
+    //     borderColor: '#d7d7d7',
+    //     borderRadius: 16,
+    //     width: '90%',
+    //     height: 150,
+    // }
 });

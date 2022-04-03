2fe5ac5bf4a49d69189ec1320788a915a406274f
Igor Shadurin <igor.shadurin@gmail.com>
2022-04-03 20:12:14 +0300

    added ethers, shims and get-random-values

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-get-random-values'
+import '@ethersproject/shims'
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
